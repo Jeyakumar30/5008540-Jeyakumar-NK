e79662df0d9cc8642d963a79eb79643a410eabd2
Jeyakumar N K <121599159+Jeyakumar30@users.noreply.github.com>
2024-08-08 03:50:15 +0530

    Delete Week 2 Spring Core Maven/LibraryManagement(9)/librarymanagement directory

--- Week 2 Spring Core Maven/LibraryManagement(9)/librarymanagement/src/main/java/com/library/LibrarymanagementSystemFull/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.library.LibrarymanagementSystemFull.repository;
-
-import com.library.LibrarymanagementSystemFull.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
