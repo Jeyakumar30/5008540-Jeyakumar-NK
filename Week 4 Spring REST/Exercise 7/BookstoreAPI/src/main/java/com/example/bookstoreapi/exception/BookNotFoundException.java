d0937ddd564a2c240fd190a2e05db61adbb35e20
Jeyakumar N K <121599159+Jeyakumar30@users.noreply.github.com>
2024-08-24 22:40:35 +0530

    Delete Week 4 Spring REST/Exercise 7/BookstoreAPI directory

--- Week 4 Spring REST/Exercise 7/BookstoreAPI/src/main/java/com/example/bookstoreapi/exception/BookNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.bookstoreapi.exception;
-
-public class BookNotFoundException extends RuntimeException {
-    public BookNotFoundException(Long id) {
-        super("Book with ID " + id + " not found.");
-    }
-}
