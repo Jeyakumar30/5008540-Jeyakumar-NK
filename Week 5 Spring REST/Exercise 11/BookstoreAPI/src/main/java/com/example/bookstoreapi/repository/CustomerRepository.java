b9cdc6c9aeb7e65236f481ab5114b205a23fed7f
Jeyakumar N K <121599159+Jeyakumar30@users.noreply.github.com>
2024-08-25 23:41:01 +0530

    Delete Week 5 Spring REST/Exercise 11 directory

--- Week 5 Spring REST/Exercise 11/BookstoreAPI/src/main/java/com/example/bookstoreapi/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.bookstoreapi.repository;
-
-import com.example.bookstoreapi.model.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
